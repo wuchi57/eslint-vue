5390661343a9b6e7f06c460c4b8f601aa0ded3ff
wuchi <wuchi57@outlook.com>
2023-09-14 18:45:06 +0800

    fix: ESLint 错误error

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
